c08df38edbc52dfae2cda83e987449d65ba2c8b8
desecnd <desecnd@protonmail.com>
2024-11-06 20:11:34 +0100

    Add fp2 add sub and mul methods

--- fp2.c
+++ fp2.c
@@ -102,3 +102,84 @@ void fp_sqrt(fp_t res, const fp_t a) {
     // step 4: clear memory for e variable
     mpz_clear(exp);
 }
+
+// =========== 
+// FP2 methods
+// ===========
+
+void fp2_init(fp2_t res) {
+    // initialize both variables
+    fp_init(res.a);
+    fp_init(res.b);
+}
+
+void fp2_clear(fp2_t res) {
+    // clear both variables
+    fp_clear(res.a);
+    fp_clear(res.b);
+}
+
+// fill: set individual fields as FP elements
+void fp2_fill(fp2_t res, fp_t a, fp_t b) {
+    fp_set(res.a, a);
+    fp_set(res.b, b);
+}
+
+// set: result = a
+void fp2_set(fp2_t res, const fp2_t arg) {
+    fp_set(res.a, arg.a);
+    fp_set(res.b, arg.b);
+}
+
+// add: result <- lhs[a + bi] + rhs[a + bi]
+void fp2_add(fp2_t res, const fp2_t lhs, const fp2_t rhs) {
+    fp_add(res.a, lhs.a, rhs.a);
+    fp_add(res.b, lhs.b, rhs.b);
+}
+
+// add: result <- lhs[a + bi] + (unsigned int) rhs
+void fp2_add_uint(fp2_t res, const fp2_t lhs, unsigned long int rhs) {
+    fp_add_uint(res.a, lhs.a, rhs);
+    fp_set(res.b, lhs.b);
+}
+
+// sub: result <- lhs[a + bi] - rhs[a + bi]
+void fp2_sub(fp2_t res, const fp2_t lhs, const fp2_t rhs) {
+    fp_sub(res.a, lhs.a, rhs.a);
+    fp_sub(res.b, lhs.b, rhs.b);
+}
+
+// sub: result <- lhs[a + bi] - rhs[a + bi]
+void fp2_sub_uint(fp2_t res, const fp2_t lhs, unsigned long int rhs) {
+    fp_sub(res.a, lhs.a, rhs);
+    fp_set(res.b, lhs.b);
+}
+
+void fp2_mul(fp2_t res, const fp2_t lhs, const fp2_t rhs) {
+    // add temprary field elements for storing values
+    fp_t tmp;
+    fp_init(tmp); // allocate and set to 0
+
+    // (a + bi) * (c + di) = [ac - bd] + [ad + bc]i
+
+    // calculate the first variable (real part); ac - bd 
+    // res[0] <- a * c
+    fp_mul(res.a, lhs.a, rhs.a);
+    // temp <- b * d
+    fp_mul(tmp, lhs.b, rhs.b);
+    // res[0] <- [res0](a * c) - [tmp](b * d)
+    fp_sub(res.a, res.a, tmp);
+
+    // calculate the second variable (real part); ad - bc
+    // res[1] <- a * d
+    fp_mul(res.b, lhs.a, rhs.b);
+    // TODO: make sure we can set the register without UB
+    // i.e. there is no garbage left from other operations
+    // so no need to set value to 0?
+
+    // tmp <- b * c
+    fp_mul(tmp, lhs.b, rhs.a);
+    fp_add(res.b, res.b, tmp);
+
+    fp_clear(tmp);
+}
